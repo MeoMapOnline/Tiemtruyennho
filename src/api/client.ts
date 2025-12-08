a43a14249206ca23328eab1216fd956c91ecdfa4
MeoMapOnline <muncute0377392767@gmail.com>
2025-12-08 23:46:31 +0700

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8787';
+const BASE_URL = https://tiemnhobackend.matoralie0789.workers.dev
 
 export const api = {
   async get(endpoint: string) {
